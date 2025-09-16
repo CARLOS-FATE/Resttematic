06da3e1ab577e1f960c9a9cc5267bb80d734377a
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-09-15 23:21:17 -0500

    chore: Finalizar configuración de Vercel

--- electrical-event/astro.config.mjs
+++ electrical-event/astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
+
 
 export default defineConfig({
   output: 'server',
   integrations: [react(), tailwind()],
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel(),
+
 });
