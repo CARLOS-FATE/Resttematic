bc4830d258d660ece3deba2263c069fe54259f4f
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-09-17 18:58:05 -0500

    Nuevo comit apis9

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
+// astro.config.mjs
+
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    rewrites: [
+      {
+        source: "/api/(.*)",
+        destination: "/api/[...catchall].js"
+      }
+    ]
+  }),
   integrations: [react(), tailwind()]
 });
