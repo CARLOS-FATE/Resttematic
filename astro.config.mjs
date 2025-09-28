7435f6f8c7cddf908c4bb1596d5a68b341b2e147
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-09-28 00:21:11 -0500

    feat: añadir URL del sitio para generar sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.vangoghfusionsetcafe.com', 
   output: "server",
   adapter: vercel(),
   integrations: [react(), tailwind(), sitemap()]
