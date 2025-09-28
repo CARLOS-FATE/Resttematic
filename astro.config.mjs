8eba1e94e58e0766c92d7337777bbdc17d0671bc
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-09-27 22:45:26 -0500

    Mapeo de sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sitemap()]
 });
