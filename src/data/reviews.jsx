12968305c5834900df1032b185807443501ba9bb
CARLOS-FATE <xsxcarloslic15@gmail.com>
2025-10-26 22:09:41 -0500

    añadir cartas

---
+++ src/data/reviews.jsx
@@ -0,0 +1,21 @@
+// src/data/reviews.js
+export const reviews = [
+  {
+    id: 1,
+    author: "Ana Gutiérrez",
+    rating: 5,
+    text: "¡Un ambiente increíble! La decoración inspirada en Van Gogh es mágica y la comida... espectacular. El lomo saltado fue una obra de arte."
+  },
+  {
+    id: 2,
+    author: "Marco Polo R.",
+    rating: 4.5,
+    text: "Excelente café y los postres son de otro nivel. Un lugar perfecto para una tarde tranquila. La atención fue muy amable."
+  },
+  {
+    id: 3,
+    author: "Lucía Fernandez",
+    rating: 5,
+    text: "¡La mejor fusión que he probado en Huamachuco! Las carnes a la parrilla tienen un sabor único. 100% recomendado."
+  }
+];
