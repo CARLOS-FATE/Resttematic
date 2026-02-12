d06654bf4396b19303db44ceb6ba65843963ecb6
CARLOS-FATE <xsxcarloslic15@gmail.com>
2026-02-11 23:33:50 -0500

    fix this nav header2

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -448,7 +448,7 @@ const AdminDashboard = ({ userRole }) => {
                 )}
             </section>
 
-            <section className="bg-white p-6 rounded-lg shadow-md">
+            <section className="bg-white p-6 rounded-lg shadow-md mb-8">
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-2xl font-semibold">Gestión de Usuarios</h2>
                     <button
@@ -663,7 +663,7 @@ const AdminDashboard = ({ userRole }) => {
 
             <section className="bg-white p-6 rounded-lg shadow-md mb-8 text-gray-800">
                 <div className="flex justify-between items-center mb-6">
-                    <h2 className="text-2xl font-semibold font-serif">Gestión de Salón</h2>
+                    <h2 className="text-2xl font-semibold ">Gestión de Salón</h2>
                     <button
                         onClick={() => setIsTableManagementVisible(!isTableManagementVisible)}
                         className="bg-gray-200 text-gray-700 py-2 px-4 rounded-md hover:bg-gray-300 font-medium transition"
